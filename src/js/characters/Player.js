c229fce02134e2b1bf591fc9c2a0eeffd2b58c81
Andrey Savka <a_sawka@yahoo.com>
2024-12-15 10:16:39 +0300

    impelement battle logic

--- src/js/characters/Player.js
+++ src/js/characters/Player.js
@@ -49,4 +49,69 @@ export class Player {
     return this.life === 0;
   }
 
+  moveLeft(distance) {
+    this.position -= Math.min(this.speed, distance);
+  }
+
+  moveRight(distance) {
+    this.position += Math.min(this.speed, distance);
+  }
+
+  move(distance) {
+    if (distance > 0) {
+      this.moveRight(distance);
+    } else {
+      this.moveLeft(-distance);
+    }
+  }
+
+  isAttackBlocked() {
+    return this.getLuck() > (100 - this.luck) / 100;
+  }
+
+  dodged() {
+    return this.getLuck() > (100 - this.agility - this.speed * 3) / 100;
+  }
+
+  takeAttack(damage) {
+    if (this.isAttackBlocked()) {
+      this.weapon.takeDamage(damage);
+    } else if (!this.dodged()) {
+      this.takeDamage(damage);
+    }
+  }
+
+  tryAttack(enemy) {
+    let distance = Math.abs(this.position - enemy.position);
+    if (distance > this.weapon.range) {
+      return;
+    }
+    this.weapon.takeDamage(10 * this.getLuck());
+    let damage = this.getDamage(distance);
+    if (distance === 0) {
+      enemy.moveRight(1);
+      damage *= 2;
+    }
+    enemy.takeAttack(damage);
+  }
+
+  chooseEnemy(players) {
+    return players
+      .filter(player => player !== this)
+      .reduce(
+        (acc, el) => acc === null || el.life < acc.life ? el : acc,
+        null
+      );
+  }
+
+  moveToEnemy(enemy) {
+    this.move(enemy.position - this.position);
+  }
+
+  turn(players) {
+    const enemy = this.chooseEnemy(players);
+    this.moveToEnemy(enemy);
+    this.tryAttack(enemy);
+  }
+
 }
--- src/js/characters/__tests__/player.test.js
+++ src/js/characters/__tests__/player.test.js
@@ -82,5 +82,107 @@ describe('Player', () => {
       expect(player.isDead()).toBe(expected);
     });
 
+    it('should move left', () => {
+      const expected = 9;
+      player.moveLeft(1);
+      expect(player.position).toBe(expected);
+    });
+
+    it('should not exceed the speed when moving left', () => {
+      const expected = 9;
+      player.moveLeft(2);
+      expect(player.position).toBe(expected);
+    });
+
+    it('should move right', () => {
+      const expected = 11;
+      player.moveRight(1);
+      expect(player.position).toBe(expected);
+    });
+
+    it('should not exceed the speed when moving right', () => {
+      const expected = 11;
+      player.moveRight(2);
+      expect(player.position).toBe(expected);
+    });
+
+    it('should choose right direction', () => {
+      const expected = 11;
+      player.move(1);
+      expect(player.position).toBe(expected);
+    });
+
+    it('should choose left direction', () => {
+      const expected = 9;
+      player.move(-1);
+      expect(player.position).toBe(expected);
+    });
+
+    it('should block attack when lucky', () => {
+      player.getLuck = () => 0.91;
+      const isBlocked = player.isAttackBlocked();
+      expect(isBlocked).toBe(true);
+    });
+
+    it('should not block attack when not lucky enough', () => {
+      player.getLuck = () => 0.89;
+      const isBlocked = player.isAttackBlocked();
+      expect(isBlocked).toBe(false);
+    });
+
+    it('should be dodged when lucky', () => {
+      player.getLuck = () => 0.93;
+      const dodged = player.dodged();
+      expect(dodged).toBe(true);
+    });
+
+    it('should not be dodged when not lucky enough', () => {
+      player.getLuck = () => 0.91;
+      const dodged = player.dodged();
+      expect(dodged).toBe(false);
+    });
+
+    it('should take attack and block it', () => {
+      player.getLuck = () => 0.95;
+      player.weapons = [ new Sword() ];
+      player.takeAttack(10);
+      expect(player.life).toBe(100);
+      expect(player.weapon.durability).toBe(490);
+    });
+
+    it('should dodge attack', () => {
+      player.getLuck = () => 0.8;
+      player.weapons = [ new Sword() ];
+      player.agility = 90;
+      player.takeAttack(10);
+      expect(player.life).toBe(100);
+      expect(player.weapon.durability).toBe(500);
+    });
+
+    it('should choose weakest enemy', () => {
+        const player2 = new Player(8, 'Никита Попович');
+        const player3 = new Player(7, 'Петр Колобкович');
+        player3.life = 50;
+        const players = [ player, player2, player3 ];
+        const choosen = player.chooseEnemy(players);
+        expect(choosen).toBe(player3);
+    });
+
+    it('should move to the enemy', () => {
+      const player2= new Player(8, 'Никита Попович');
+      player.moveToEnemy(player2);
+      expect(player.position).toBe(9);
+    });
+
+    it('should move on turn to weakest enemy', () => {
+      const player2 = new Player(8, 'Никита Попович');
+      const player3 = new Player(14, 'Петр Колобкович');
+      player3.life = 50;
+      const players = [ player, player2, player3 ];
+      const choosen = player.turn(players);
+      expect(player.position).toBe(11);
+  });
+
+
   });
 });
